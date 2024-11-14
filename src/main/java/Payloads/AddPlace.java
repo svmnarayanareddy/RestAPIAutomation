96ba23097c8ec255233fa37b0526c1d847cf2ebd
svmnarayanareddy <svmnarayanareddy@gmail.com>
2024-11-14 12:06:47 +0530

    RestAussered

---
+++ src/main/java/Payloads/AddPlace.java
@@ -0,0 +1,25 @@
+package Payloads;
+
+public class AddPlace {
+
+	public static String AddNewPlace() {
+		// TODO Auto-generated method stub
+		return "{\r\n"
+				+ "  \"location\": {\r\n"
+				+ "    \"lat\": -38.383494,\r\n"
+				+ "    \"lng\": 33.427362\r\n"
+				+ "  },\r\n"
+				+ "  \"accuracy\": 50,\r\n"
+				+ "  \"name\": \"SVSLS Travels\",\r\n"
+				+ "  \"phone_number\": \"(+91) 9849303327\",\r\n"
+				+ "  \"address\": \"N.S.P Colony chimakurthy\",\r\n"
+				+ "  \"types\": [\r\n"
+				+ "    \"Tansoports\",\r\n"
+				+ "    \"House constraction materials\"\r\n"
+				+ "  ],\r\n"
+				+ "  \"website\": \"http://svlstransports.com\",\r\n"
+				+ "  \"language\": \"English-IN\"\r\n"
+				+ "}";
+	}
+
+}
---
+++ src/main/java/REST/BasicRestAutomation.java
@@ -0,0 +1,40 @@
+package REST;
+
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+import Payloads.AddPlace;
+
+public class BasicRestAutomation {
+	public static void main(String[] args) {
+		
+		//Given based on Input Details
+		RestAssured.baseURI = "https://rahulshettyacademy.com";
+		String Response = given().log().all().queryParam("key", "qaclick123").headers("Content-Type", "application/json")
+		.body(AddPlace.AddNewPlace()).when().post("maps/api/place/add/json")
+		.then().log().all().assertThat().statusCode(200).body("status", equalTo("OK")).extract().asString();
+		System.out.println(Response);
+		JsonPath js = new JsonPath(Response);
+		String PlaceId = js.getString("place_id");
+		System.out.println(PlaceId);
+		
+		//PUT Method
+		given().log().all().queryParam("key", "qaclick123").headers("Content-Type", "application/json")
+		.body("{\r\n"
+				+ "\"place_id\":\""+PlaceId+"\",\r\n"
+				+ "\"address\":\"70 Summer walk, USA\",\r\n"
+				+ "\"key\":\"qaclick123\"\r\n"
+				+ "}").when().put("maps/api/place/update/json")
+		.then().log().all().assertThat().statusCode(200).body("msg", equalTo("Address successfully updated"));
+		
+		//Get Method
+		String AdressData = given().queryParam("key", "qaclick123").queryParam("place_id", PlaceId).when().get("maps/api/place/get/json")
+		.then().log().all().assertThat().statusCode(200).extract().response().asString();
+		JsonPath Data = new JsonPath(AdressData);
+		String GetAdress = Data.getString("address");
+		System.out.println("getData: "+ GetAdress);
+	}
+}
